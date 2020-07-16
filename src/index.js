d2f7e377f28cd3944ffe1e914a46be95abeeac6f
B-Rad <lee078bg@yahoo.com>
2020-07-15 23:44:47 -0700

    Added redux persist to the project since if and when you refresh the page, all the items in the page disappear.  Redux persist is used to keep the state of the application.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import * as serviceWorker from './serviceWorker';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
-	<Provider store={configureStore()}>
-	    <App />
+	<Provider store={store}>
+		<PersistGate 
+			loading={
+				<div>
+					Loading...
+				</div>
+			}
+			persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,23 @@
 import { createStore, combineReducers } from 'redux';
-import { todos } from './todos/reducers'
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { todos } from './todos/reducers';
 
 const reducers = {
 	todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+	key: 'root',
+	storage,
 
-export const configureStore = () => createStore(rootReducer);
+	// Tells Redux how to reconcile initial and stored states
+	// of our application
+	stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
