e9dadd2ec7d3207d24d487cf186973edddea8a64
B-Rad <lee078bg@yahoo.com>
2020-07-15 22:12:59 -0700

    Added reducers along with redux code that does not currently have any function at this time.

---
+++ src/reducers.js
@@ -0,0 +1,24 @@
+import { CREATE_TODO, REMOVE_TODO } from './actions';
+
+
+export const todos = (state = [], action) => {
+	const { type, payload } = action;
+
+	switch(type) {
+		case CREATE_TODO: {
+			const { text } = payload;
+			const newToDo = {
+				text,
+				isCompleted: false,
+			};
+
+			return state.concat(newToDo);
+		}
+		case REMOVE_TODO: {
+			const { text } = payload;
+            return state.filter(todo => todo.text !== text);
+		}
+        default:
+            return state;
+	}
+};
